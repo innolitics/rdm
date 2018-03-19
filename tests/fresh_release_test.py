1c0cd76ff305ec0ab39f18522d0e732121467371
J. David Giese <johndgiese@gmail.com>
2018-03-19 18:57:08 -0400

    Print directory contents

--- tests/fresh_release_test.py
+++ tests/fresh_release_test.py
@@ -5,4 +5,5 @@ import subprocess
 def test_building_fresh_release():
     init_directory = tempfile.mkdtemp()
     subprocess.check_call(['rdm', 'init', init_directory])
+    subprocess.check_call(['ls', '-la', init_directory])
     subprocess.check_call(['make'], cwd=init_directory)

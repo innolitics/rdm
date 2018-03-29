95778f6005189ddf52ecf1cd191e731f3795fdb8
Reece Stevens <reecestevens24@gmail.com>
2018-03-29 18:33:08 -0500

    Initial pass through dumping issues into yaml files
    See #3

---
+++ rdm/requirements.py
@@ -0,0 +1,34 @@
+'''
+Functions that parse the given project management service and produce
+`requirements.yml` and `problem_reports.yml` files.  The selected project
+management backend is specified in the project configuration file.
+'''
+import os
+
+from github import Github
+import yaml
+
+# TODO: Move this to a project-wide configuration file for rdm.
+# Should that file be merged with the `system.yml` file?
+GH_API_TOKEN = os.getenv('GH_API_TOKEN')
+REPOSITORY = 'innolitics/freesurfer'
+requirements_path = './requirements.yml'
+problem_reports_path = './problem_reports.yml'
+
+def get_requirements_from_github():
+    g = Github(GH_API_TOKEN)
+    repository = g.get_repo(REPOSITORY)
+    requirements = {idx: {'title': issue.title, 'description': issue.body.replace('\r', '')}
+                    for idx, issue in enumerate(repository.get_issues())
+                    if 'requirement' in issue.labels}
+
+    problem_reports = {idx: {'title': issue.title, 'description': issue.body.replace('\r', '')}
+                       for idx, issue in enumerate(repository.get_issues())
+                       if 'problem-report' in issue.labels}
+    with open(requirements_path, 'w') as f:
+        yaml.dump(requirements, f, default_flow_style=False)
+    with open(problem_reports_path, 'w') as f:
+        yaml.dump(problem_reports, f, default_flow_style=False)
+
+if __name__ == '__main__':
+    get_requirements_from_github()

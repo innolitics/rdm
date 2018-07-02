81aad78c8e97167f7549327134a1a95cd10f7b82
Russell <txrjk96@utexas.edu>
2018-07-01 19:21:03 -0500

    Add multiple issue processing capability

--- tests/hook_test.py
+++ tests/hook_test.py
@@ -7,37 +7,61 @@ from git import Repo
 
 @pytest.fixture
 def tmp_repo(tmpdir):
-	repository = Repo.init(tmpdir)
+	# initialize new repository and change working directory
+	repo = Repo.init(tmpdir)
+	os.chdir(tmpdir)
 
 	# set up logging and trace
 	logging.basicConfig()
 	logging.root.setLevel(logging.INFO)
-	type(repository.git).GIT_PYTHON_TRACE='full'
+	type(repo.git).GIT_PYTHON_TRACE='full'
 
+	# create initial commit
 	file_path = os.path.join(tmpdir, 'initial-commit.txt')
 	subprocess.call(['touch', file_path])
-	repository.git.add('--all')
-	repository.git.commit('-m', '\'message\'', '--no-verify')
+	repo.git.add('--all')
+	repo.git.commit('-m', '\'message\'', '--no-verify')
 
 	subprocess.call(['rdm', 'hooks', tmpdir + '/.git/hooks'])
 	
-	yield repository, tmpdir
+	yield repo
 
+	# teardown
 	subprocess.call(['rm', '-rf', tmpdir])
 
-def test_single_issue(tmp_repo):
-	repo = tmp_repo[0]
-	directory = tmp_repo[1]
 
-	prepare_branch(repo, 'feature/10-sample-issue', directory)
+def prepare_branch(tmp_repo, branch_name):
+	directory = os.getcwd()
 
-	assert subprocess.check_output(['git', 'show', '-s', '--format=%B'], cwd=directory, encoding='utf-8') == "Fix some issue\n\nIssue #10\n\n"
-
-def prepare_branch(repo, branch_name, directory):
-	repo.git.checkout('-b', branch_name)
+	tmp_repo.git.checkout('-b', branch_name)
 
 	file_path = os.path.join(directory, 'empty-file.txt')
 	subprocess.call(['touch', file_path])
-	
-	repo.git.add('--all')
-	repo.git.commit('-m', 'Fix some issue')
+
+	tmp_repo.git.add('--all')
+	tmp_repo.git.commit('-m', 'Fix some issue')
+
+
+def test_single_issue(tmp_repo):
+	prepare_branch(tmp_repo, '10-sample-issue')
+
+	assert subprocess.check_output(['git', 'show', '-s', '--format=%B'], encoding='utf-8') == "Fix some issue\n\nIssue #10\n\n"
+
+
+def test_multiple_issues(tmp_repo):
+	prepare_branch(tmp_repo, '10-11-sample-issue')
+
+	assert subprocess.check_output(['git', 'show', '-s', '--format=%B'], encoding='utf-8') == "Fix some issue\n\nIssue #10\n\nIssue #11\n\n"
+
+
+def test_text_before_issue(tmp_repo):
+	prepare_branch(tmp_repo, 'fix-10-sample-issue')
+
+	assert subprocess.check_output(['git', 'show', '-s', '--format=%B'], encoding='utf-8') == "Fix some issue\n\nIssue #10\n\n"
+
+
+def test_no_issue_number(tmp_repo):
+	with pytest.raises(Exception) as Error:
+		prepare_branch(tmp_repo, 'sample-issue')
+
+	assert "Aborting commit. Your commit message is missing either a Github Issue ('Issue #131') or 'Merge'" in str(Error.value)

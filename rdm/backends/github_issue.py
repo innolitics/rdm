5b0db4db561516b42568693f41431399e780beb9
J. David Giese <johndgiese@gmail.com>
2019-03-12 19:09:49 -0400

    Make flake happy

--- rdm/backends/github_issue.py
+++ rdm/backends/github_issue.py
@@ -14,6 +14,7 @@ def pull(system):
 #
 # issue
 
+
 def _pull_from_repository(github_repository):
     # TODO: only grab issues for the current release
     issues = [i for i in github_repository.get_issues(state='all') if not _is_obsolete(i)]
@@ -21,11 +22,11 @@ def _pull_from_repository(github_repository):
     # get all pull requests that are:
     # 1. merged into master
     # 2. during the release
-    # 
+    #
     # for each pull request
     # 1. find the associated change requests
-    # 2. 
-    pull_requests = list(github_repository.get_pulls(state='closed'))
+    # 2.
+    # pull_requests = list(github_repository.get_pulls(state='closed'))
     print_info('Pulled {} issues from {}'.format(len(issues), github_repository.url))
     problem_reports = [clean_issue(i) for i in issues if _is_problem_report(i)]
     change_requests = [clean_issue(i) for i in issues if _is_change_request(i)]
--- rdm/backends/github_pr.py
+++ rdm/backends/github_pr.py
@@ -7,6 +7,7 @@ from rdm.backends.github_base import authenticate_github
 def pull(system):
     github_browser = authenticate_github()
     github_repository = github_browser.get_repo(system['repository'])
+    github_repository.get_issues(state='all')
     # TODO: implement code that grabs
     # 1. change requests
     # 2. problem reports

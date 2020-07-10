339c777d48e6a9e2d14bb49b29fe56c46e764210
J. David Giese <johndgiese@gmail.com>
2020-07-10 16:35:15 -0400

    Use ABC for ProjectManagement Backends

--- rdm/project_management/base.py
+++ rdm/project_management/base.py
@@ -1,6 +1,10 @@
-class BaseBackend:
+from abc import ABC, abstractmethod
+
+
+class BaseBackend(ABC):
     def __init__(self, config):
         self.config = config
 
+    @abstractmethod
     def pull(self):
-        raise NotImplementedError()
+        ...

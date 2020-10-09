6ba70d08490fc0057611c99c64434b5cee44f5b9
J. David Giese <johndgiese@gmail.com>
2020-10-09 19:42:51 -0400

    Bump setup.py version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ description = 'Regulatory Documentation Manager'
 
 setup(
     name='rdm',
-    version='0.9.1',
+    version='0.9.2',
     description=description,
     long_description=description,
     url='https://github.com/innolitics/rdm',

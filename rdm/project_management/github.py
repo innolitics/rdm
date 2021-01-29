e22be05366d7b271275a4b88c341b77bbb6cd20e
Daniel Heater <daniel@heatertech.com>
2021-01-28 21:01:37 -0600

    Fix broken link to GH_API_TOKEN documentation. Fixes #72

--- rdm/project_management/github.py
+++ rdm/project_management/github.py
@@ -30,7 +30,7 @@ def authenticate_github():
         print_info('No access token is stored in the GH_API_TOKEN environment variable.')
         help_url = 'https://help.github.com/en/articles/' + \
             'creating-a-personal-access-token-for-the-command-line'
-        print_info('See ' + help_url + 'for details.')
+        print_info('See ' + help_url + ' for details.')
         print_info('Defaulting to username / password for login.')
         username = input('GitHub username: ')
         password = getpass('GitHub password (will not echo to console): ')
